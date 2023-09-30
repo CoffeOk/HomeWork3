d0ffaeb2ec73854d066610604722172892f48168
Wall-e <you@mail.com>
2023-10-01 01:18:51 +0300

    Reshenie zadachi 23

---
+++ Task03(23)/Program.cs
@@ -0,0 +1,19 @@
+﻿/* На вход число num
+На выходе таблица кубов от 1 до n */
+
+
+void TableKub (int num)
+{
+    for (int i = 0; i <= num; i++)
+    {
+        Console.WriteLine ($"{i,3} | {Math.Pow (i,3), 6}");
+    }
+}
+
+int n;
+
+Console.Write ("Введите целое число n: ");
+while (!int.TryParse (Console.ReadLine(), out n))
+    Console.Write ("Ошибка ввода. Введите целое число: ");
+
+TableKub (n);
