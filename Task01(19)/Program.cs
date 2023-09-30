741d5eaff138e3c83afa2236e44c1a76b486c205
Wall-e <you@mail.com>
2023-10-01 00:04:19 +0300

    Reshenie zadachi 19

---
+++ Task01(19)/Program.cs
@@ -0,0 +1,29 @@
+﻿/* Принимает пятизначное число
+Проверяет является ли оно полиндромом */
+
+
+bool Polindrom(int num)
+{
+    int revers, x;
+    revers = 0;
+    while (num != 0)
+    {
+        x = num % 10;
+        revers = revers * 10 + x;
+        num /= 10;
+    }
+    if (num != revers) return false;
+    return true;
+}
+
+int n;
+
+Console.Write("Введите целое число n: ");
+while(!int.TryParse(Console.ReadLine(), out n))
+    Console.Write("Ошибка ввода. Введите целое число: ");
+
+bool number = Polindrom(n);
+if (number != true)
+    Console.Write($"{n} -> Да");
+else
+    Console.Write($"{n} -> Нет");
